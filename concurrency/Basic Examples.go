79cff30bffb7eb340340ac160495e40e43d0f8ea
Wang Yaoshen <wangyaoshen@bgi.com>
2019-03-21 11:01:44 +0800

    use select rewrite fan-in func with single goroutine

--- concurrency/Basic Examples.go
+++ concurrency/Basic Examples.go
@@ -63,12 +63,12 @@ func fanIn(input1, input2 <-chan Message) <-chan Message {
 	c := make(chan Message)
 	go func() {
 		for {
-			c <- <-input1
-		}
-	}()
-	go func() {
-		for {
-			c <- <-input2
+			select {
+			case s := <-input1:
+				c <- s
+			case s := <-input2:
+				c <- s
+			}
 		}
 	}()
 	return c
